0a83147542967e0398610d39f336f620ad88c08a
Melvin Otieno <o.melvinotieno@gmail.com>
2022-06-23 13:22:25 +0300

    autoload the created components

---
+++ lib/formish/components.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Formish
+  module Components
+    extend ActiveSupport::Autoload
+    
+    autoload :Error
+    autoload :Hint
+    autoload :Input
+    autoload :label
+  end
+end
