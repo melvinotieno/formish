64d8508f329b5597ea224ca80cbf9538b9c27297
Melvin Otieno <o.melvinotieno@gmail.com>
2022-06-23 13:06:16 +0300

    add auto_load, configured property and mattr_accessors

--- lib/formish.rb
+++ lib/formish.rb
@@ -1,6 +1,33 @@
+# frozen_string_literal: true
+
 require "formish/version"
 require "formish/railtie"
+require "formish/action_view_extensions/form"
 
 module Formish
-  # Your code goes here...
+  extend ActiveSupport::Autoload
+
+  eager_autoload do
+    autoload :FormBuilder
+  end
+
+  @@configured = false
+
+  # Check if Formish is configured
+  def self.configured?
+    @@configured
+  end
+
+  # Setup Formish
+  def self.setup
+    @@configured = true
+    yield self
+  end
+
+  ## CONFIGURATION OPTIONS
+
+  mattr_accessor :field_error_proc
+  @@field_error_proc = proc do |html_tag, instance_tag|
+    html_tag
+  end
 end
